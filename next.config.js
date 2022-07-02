504f82a73e12a45d6893bbf324a1ed2b69120cb9
madeleineenberg <madeleine_enberg@hotmail.com>
2022-07-02 13:29:18 +0200

    add: dotenv ant auth0 package

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname)
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.plugins.push(new Dotenv({ silent: true }));
     return config;
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
