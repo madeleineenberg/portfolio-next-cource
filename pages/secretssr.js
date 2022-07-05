fd7708151967ac972a5979e081d67057367c844e
madeleineenberg <madeleine_enberg@hotmail.com>
2022-07-05 10:16:08 +0200

    Auth on server side

--- pages/secretssr.js
+++ pages/secretssr.js
@@ -1,21 +1,35 @@
 import BaseLayout from '@/components/shared/BaseLayout';
 import BasePage from '@/components/BasePage';
-import { authorizeUser } from '@/utils/auth0';
+import { authorizeUser, withAuth } from '@/utils/auth0';
 
-export default function SecretSSR({ user }) {
+export default function SecretSSR({ user, title }) {
   return (
     <BaseLayout user={user} loading={false}>
       <BasePage>
         <h1>I am SecretSSR Page - Hello {user && user.name}</h1>
+        <h2>{title}</h2>
       </BasePage>
     </BaseLayout>
   );
 }
 
-export const getServerSideProps = async ({ req, res }) => {
-  const user = await authorizeUser(req, res);
+// export const getServerSideProps = async ({ req, res }) => {
+//   const user = await authorizeUser(req, res);
 
-  return {
-    props: { user },
-  };
+//   return {
+//     props: { user },
+//   };
+// };
+
+const getTitle = () => {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res({ title: 'My new title' });
+    });
+  }, 500);
 };
+
+export const getServerSideProps = withAuth(async ({ req, res }, user) => {
+  const title = await getTitle();
+  return title;
+});
