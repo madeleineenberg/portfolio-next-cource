6eb1b6e1d3da679ceb3260df93b9e4fcfc7f0289
madeleineenberg <madeleine_enberg@hotmail.com>
2022-08-17 15:20:11 +0200

    get portfolios

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,33 +1,42 @@
-
-import BaseLayout from '@/components/shared/BaseLayout'
+import BaseLayout from '@/components/shared/BaseLayout';
 import Link from 'next/link';
 import BasePage from '@/components/BasePage';
-import { useGetPosts} from '@/actions';
-import {useGetUser} from '@/actions/user';
+import { useGetUser } from '@/actions/user';
+import PortfolioApi from '@/lib/api/portfolios';
 
-export default function Portfolios() {
-const { data, error, loading } = useGetPosts();
-const {data: dataU, loading: loadingU} = useGetUser();
- 
+export default function Portfolios({ portfolios }) {
+  const { data: dataU, loading: loadingU } = useGetUser();
+
   return (
     <BaseLayout user={dataU} loading={loadingU}>
       <BasePage>
         <h1>This is the Portfolio page</h1>
-        {loading && <p>Loading data...</p>}
+
         <ul>
-          {data &&
-            data.map((post) => {
+          {portfolios &&
+            portfolios.map((portfolio) => {
               return (
-                <li key={post.id}>
-                  <Link href={`/portfolios/${post.id}`}> 
-                    <a>{post.title}</a>
+                <li key={portfolio._id}>
+                  <Link
+                    as={`/portfolios/${portfolio._id}`}
+                    href={'/portfolios/[id]'}
+                  >
+                    <a>{portfolio.title}</a>
                   </Link>
                 </li>
               );
             })}
         </ul>
-        {error && <div className='alert alert-danger'>{error.message}</div>}
       </BasePage>
     </BaseLayout>
   );
+}
+
+export async function getStaticProps() {
+  const json = await new PortfolioApi().getAll();
+  const portfolios = json.data;
+
+  return {
+    props: { portfolios },
+  };
 }
